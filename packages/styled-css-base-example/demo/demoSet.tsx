ba4ac9b2f9d7a641fae4a018af7bc5d93f9e6299
Zheeeng <hi@zheeeng.me>
2022-07-15 18:20:12 +0800

    feat: add more table styles

--- packages/styled-css-base-example/demo/demoSet.tsx
+++ packages/styled-css-base-example/demo/demoSet.tsx
@@ -110,36 +110,109 @@ export const demoSet = {
     ),
     Table: () => (
         <table>
-            <tr>
-                <th>Name</th>
-                <th>Author</th>
-                <th>Subject</th>
-                <th>Publication Year</th>
-            </tr>
-            <tr>
-                <td>万历十五年</td>
-                <td>黃仁宇</td>
-                <td>History</td>
-                <td>1981</td>
-            </tr>
-            <tr>
-                <td>{'L\'Ancien Régime et la Révolution'}</td>
-                <td>Alexis de Tocqueville</td>
-                <td>History</td>
-                <td>1866</td>
-            </tr>
-            <tr>
-                <td>A Brief History of Time</td>
-                <td>Stephen Hawking</td>
-                <td>Cosmology</td>
-                <td>1988</td>
-            </tr>
-            <tr>
-                <td>失楽園</td>
-                <td>渡辺淳一</td>
-                <td>Novel</td>
-                <td>1995</td>
-            </tr>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Author</th>
+                    <th>Subject</th>
+                    <th>Publication Year</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>万历十五年</td>
+                    <td>黃仁宇</td>
+                    <td>History</td>
+                    <td>1981</td>
+                </tr>
+                <tr>
+                    <td>{'L\'Ancien Régime et la Révolution'}</td>
+                    <td>Alexis de Tocqueville</td>
+                    <td>History</td>
+                    <td>1866</td>
+                </tr>
+                <tr>
+                    <td>A Brief History of Time</td>
+                    <td>Stephen Hawking</td>
+                    <td>Cosmology</td>
+                    <td>1988</td>
+                </tr>
+                <tr>
+                    <td>失楽園</td>
+                    <td>渡辺淳一</td>
+                    <td>Novel</td>
+                    <td>1995</td>
+                </tr>
+            </tbody>
+        </table>
+    ),
+    TableVertical: () => (
+        <table>
+            <thead>
+                <tr>
+                    <th>Continent</th>
+                    <th>Population(2020)</th>
+                    <th>Area(Km²)</th>
+                    <th>Density(P/Km²)</th>
+                    <th>World Population Share</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Asia</th>
+                    <td>3,261,050,390</td>
+                    <td>31,033,131</td>
+                    <td>105</td>
+                    <td>41.84%</td>
+                </tr>
+                <tr>
+                    <th>Africa</th>
+                    <td>1,340,598,147</td>
+                    <td>29,648,481</td>
+                    <td>45</td>
+                    <td>17.20%</td>
+                </tr>
+                <tr>
+                    <th>Europe</th>
+                    <td>747,636,026</td>
+                    <td>22,134,900	</td>
+                    <td>34</td>
+                    <td>9.59%</td>
+                </tr>
+                <tr>
+                    <th>North America</th>
+                    <td>592,072,212</td>
+                    <td>21,330,000</td>
+                    <td>28</td>
+                    <td>7.60%</td>
+                </tr>
+                <tr>
+                    <th>South America</th>
+                    <td>430,759,766</td>
+                    <td>17,461,112</td>
+                    <td>25</td>
+                    <td>5.53%</td>
+                </tr>
+                <tr>
+                    <th>Australia/Oceania</th>
+                    <td>43,111,704</td>
+                    <td>8,486,460</td>
+                    <td>5</td>
+                    <td>0.55%</td>
+                </tr>
+                <tr>
+                    <th>Antarctica</th>
+                    <td>0</td>
+                    <td>13,720,000</td>
+                    <td>0</td>
+                    <td>0.00%</td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colSpan={5}>Reference: <a href="https://www.worldometers.info/geography/7-continents/">here</a></td>
+                </tr>
+            </tfoot>
         </table>
     ),
     Ul: () => (
@@ -228,6 +301,7 @@ export const createDemoSet = ({ preCode }: { preCode: string }) => (
                 </>
             )}
             {demoSet.Section(demoSet.Table())}
+            {demoSet.Section(demoSet.TableVertical())}
             {demoSet.Section(
                 <>
                     {demoSet.Section(demoSet.Ul())}
